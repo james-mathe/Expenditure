436d4cc452cd6102095dd128dd4fa5a3556b2b03
james-mathe <kambalemathejacques3o@gmail.com>
2024-09-18 14:40:26 +0200

    crud Expenditure and authentication

---
+++ app/Http/Controllers/ExpenditureController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ExpenditureResource;
+use App\Models\Expenditure;
+use App\Http\Requests\StoreExpenditureRequest;
+use App\Http\Requests\UpdateExpenditureRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ExpenditureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $expenditure = Expenditure::get();
+        if($expenditure->count() > 0){
+            return [
+                "Expenditures"=> ExpenditureResource::collection($expenditure)
+            ];
+        }
+        else{
+            return response()->json([
+                "message"=> "No Expenditure found"
+            ]);
+        }
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $isValide = Validator::make($request->all(),[
+            "name"=> "required",
+            "amount"=> "required|integer",
+        ]);
+
+        if($isValide->fails()){
+            return response()->json([
+                "error"=> $isValide->messages()
+            ]);
+        }
+
+        $expenditure = Expenditure::create([
+            "name"=> $request->name,
+            "amount"=> $request->amount
+        ]);
+        return response()->json([
+            "message"=> "You're Spending monney"
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Expenditure $expenditure)
+    {
+       
+        return new ExpenditureResource($expenditure);
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Expenditure $expenditure)
+    {
+        $isValide = Validator::make($request->all(),[
+            "name"=> "required",
+            "amount"=> "required|integer"
+        ]);
+        if($isValide->fails()){
+            return response()->json([
+                "error"=> $isValide->messages()
+            ]);
+        }
+        $expenditure->update([
+            "name"=> $request->name,
+            "amount"=> $request->amount
+        ]);
+        return response()->json([
+            "message"=> "Expenditure Updated Successfully"
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Expenditure $expenditure)
+    {
+        $expenditure->delete();
+        return response()->json([
+            "message"=> "Expenditure Deleted"
+        ]);
+    }
+}
